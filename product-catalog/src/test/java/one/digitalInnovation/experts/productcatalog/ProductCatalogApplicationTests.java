5c84bafe3e28919807d5cdb47bf0d275a99414f7
xXHachimanXx <globaldep90@gmail.com>
2021-09-02 21:33:25 -0300

    Starting the project

---
+++ product-catalog/src/test/java/one/digitalInnovation/experts/productcatalog/ProductCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalInnovation.experts.productcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
