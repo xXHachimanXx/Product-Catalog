fc814df3681621b1a05f546868085dfcd799c3ea
xXHachimanXx <globaldep90@gmail.com>
2021-09-07 11:50:13 -0300

    Creating config server

---
+++ config-server/src/main/java/one/digitalInnovation/experts/configserver/ConfigServerApplication.java
@@ -0,0 +1,14 @@
+package one.digitalInnovation.experts.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
